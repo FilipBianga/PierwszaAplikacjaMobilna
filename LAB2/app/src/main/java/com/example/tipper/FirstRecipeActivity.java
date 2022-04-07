d59e60d1fb14e18efabce4f4141f89a385e75c4e
Filip Bianga <filipbianga@MacBook-Air-Filip.local>
2022-04-07 10:22:53 +0200

    Add 2 Recipe

---
+++ LAB2/app/src/main/java/com/example/tipper/FirstRecipeActivity.java
@@ -0,0 +1,50 @@
+package com.example.tipper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class FirstRecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_recipe);
+
+
+        TextView textView = (TextView) findViewById(R.id.gradients);
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        textView.setText("Składniki:\n" +
+                "1 mały kalafior\n" +
+                "1 marchew\n" +
+                "mała pietruszka\n" +
+                "2 ziemniaki\n" +
+                "sól\n" +
+                "pieprz\n" +
+                "1 czubata łyżka masła\n" +
+                "ok. 700 ml bulionu drobiowego\n" +
+                "1 ząbek czosnku\n" +
+                "150 ml śmietany 18-22%\n" +
+                "szczypta cukru\n" +
+                "duża szczypta gałki muszkatołowej świeżo tartej\n" +
+                "50 g mascarpone\n" +
+                "Ponadto:\n" +
+                "½ opakowania ciasta francuskiego\n" +
+                "1 żółtko\n" +
+                "½ szklanki startego parmezanu\n" +
+                "świeży koperek, tymianek – do podania\n\n"+
+                "Przygotowanie:\n" +
+                "Kalafior dzielimy na małe różyczki, marchew i pietruszkę obieramy. Marchew zostawiamy w całości, a pietruszkę i ziemniaki kroimy w dużą kostkę. W garnku rozgrzewamy masło, wrzucamy na nie kalafior i pietruszkę. Podsmażamy chwilę, dorzucamy ziemniaki, marchew i zalewamy bulionem. Gotujemy do czasu, aż warzywa będą miękkie.\n" +
+                "\n" +
+                "W tym czasie przygotowujemy paluchy z ciasta francuskiego. Piekarnik nagrzewamy do 200 stopni. Rozkładamy ciasto francuskie na blacie lub stolnicy, kroimy w paski o szerokości 1 cm i długości ok. 15 cm, najlepiej za pomocą radełka. Bierzemy 3 paski, sklejamy je dość mocno na górze i zaplatamy warkoczyk. Smarujemy rozkłóconym jajem, posypujemy parmezanem i układamy na blaszce wyłożonej papierem do pieczenia. Wstawiamy do pieca na ok. 10 -15 minut do ładnego zrumienienia.\n" +
+                "\n" +
+                "Wyławiamy marchew z zupy, studzimy i kroimy w półplasterki. Do zupy dodajemy mascarpone, przeciskamy ząbek czosnku, doprawiamy solą i pieprzem i miksujemy na krem. Hartujemy śmietankę i dolewamy do zupy. Ciepły krem serwujemy posypany ziołami z paluchem z ciasta francuskiego.\n\n\n\n\n");
+    }
+
+
+}
--- LAB2/app/src/main/java/com/example/tipper/RecipeActivity.java
+++ LAB2/app/src/main/java/com/example/tipper/RecipeActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class RecipeActivity extends AppCompatActivity {
     Button btnBack;
+    ImageButton firstRecipe, secondRecipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,24 @@ public class RecipeActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(RecipeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        firstRecipe = (ImageButton) findViewById(R.id.firstRecipe);
+        firstRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecipeActivity.this, FirstRecipeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        secondRecipe = (ImageButton) findViewById(R.id.secondRecipe);
+        secondRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecipeActivity.this, SecondRecipeActivity.class);
                 startActivity(intent);
             }
         });
